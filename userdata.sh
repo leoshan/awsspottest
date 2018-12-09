a14294355d18c57401c24197a8f958dbda459cbf
Leo <leoshch@gmail.com>
2018-12-09 06:57:07 +0000

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 sudo su
-yum upgrade -y
 #mount efs storage and git test script
 yum install -y amazon-efs-utils git fio
 mkdir /mnt/efs
 mount -t efs fs-05b8b94f:/ /mnt/efs
 cd /mnt/efs
 git clone https://github.com/leoshan/awsnitrotest.git
+yum upgrade -y
