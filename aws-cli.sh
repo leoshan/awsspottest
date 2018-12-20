a6bf1d6356c67290cdac50f3e18727774f374317
Leo <leoshch@gmail.com>
2018-12-20 16:36:28 +0000

    Update aws-cli.sh

--- aws-cli.sh
+++ aws-cli.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #Create spot instance use config file
-aws ec2 request-spot-fleet --spot-fleet-request-config file://config.json
+#aws ec2 request-spot-fleet --spot-fleet-request-config file://config.json
+
+aws ec2 request-spot-fleet --spot-fleet-request-config file://c5.json
